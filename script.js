b8a059f2338657f57e2527a8151b733638136be0
Oratory-league <info@oratoryleague.org>
2024-11-12 00:49:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const countdownDate = new Date("Dec 8, 2024 00:00:00").getTime();
+const timer = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
